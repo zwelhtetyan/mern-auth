6943995017558e5d2def0a7a6f13b393a8457455
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-07-05 13:06:24 +0630

    build(commitizen): setting up commitizen and husky

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from "express";
+import env from "dotenv";
+env.config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => res.send("HI"));
+
+app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
