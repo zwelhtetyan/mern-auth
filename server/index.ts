1fe6ea901814d9fd111737edde3f4b88d7130e51
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-07-05 15:27:18 +0630

    connect mongodb

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,9 @@
-import express, { Request, Response } from "express";
 import env from "dotenv";
+env.config();
+import express, { Request, Response } from "express";
 import userRouter from "./routes/users/user.route";
 import { errorHandler, notFound } from "./middleware/error.middleware";
-env.config();
+import { connectMongo } from "./lib/mongo";
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,4 +16,9 @@ app.use("/api/users", userRouter);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
+const initServer = async () => {
+  await connectMongo();
+  app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
+};
+
+initServer();
---
+++ server/lib/mongo.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectMongo = async () => {
+  try {
+    if (process.env.MONGO_URI) {
+      const connectInfo = await mongoose.connect(process.env.MONGO_URI);
+      console.log(`MongoDB Connected: ${connectInfo.connection.host}`);
+    }
+  } catch (err: any) {
+    console.log(`Error: ${err.message}`);
+  }
+};
