32788ac4472b83591956d8cc117778842c863875
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-07-08 00:23:21 +0630

    build(production): build for production

--- client/src/store/api/index.ts
+++ client/src/store/api/index.ts
@@ -3,5 +3,5 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const ROOT_API = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "" }),
   tagTypes: ["user"],
-  endpoints: (builder) => ({}),
+  endpoints: () => ({}),
 });
--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ import userRouter from "./routes/users/user.route";
 import { errorHandler, notFound } from "./middleware/error.middleware";
 import { connectMongo } from "./lib/mongo";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,9 +13,18 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => res.send("HI"));
-
 app.use("/api/users", userRouter);
+
+if (process.env.NODE_ENV === "production") {
+  const dirname = path.resolve();
+  app.use(express.static(path.join(dirname, "client", "dist")));
+
+  app.get("*", (req: Request, res: Response) =>
+    res.sendFile(path.join(dirname, "client", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req: Request, res: Response) => res.send("HI"));
+}
 
 app.use(notFound);
 app.use(errorHandler);
